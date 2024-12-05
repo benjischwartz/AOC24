39122a5af4c41cd6b535057205f05d66144c4485
Benji Schwartz <benji.schwartz2013@gmail.com>
2024-12-05 13:43:09 +1100

    Refactor Part 1

--- Day4/Part1/main.cpp
+++ Day4/Part1/main.cpp
@@ -4,80 +4,18 @@
 #include <string>
 #include <vector>
 
-enum class Direction {
-    Up,
-    Down,
-    Left,
-    Right,
-    UpLeft,
-    UpRight,
-    DownLeft,
-    DownRight
-};
-
-std::pair<int, int> get_next_coord(std::pair<int, int> coord,
-                                   Direction direction)
+std::string XMAS = "XMAS";
+template <typename Callable>
+bool func(std::vector<std::vector<char>> &matrix, std::pair<int, int> coords,
+          int idx, Callable update_lambda)
 {
-    switch (direction) {
-        case Direction::Up:
-            return {coord.first - 1, coord.second};
-        case Direction::Down:
-            return {coord.first + 1, coord.second};
-        case Direction::Left:
-            return {coord.first, coord.second - 1};
-        case Direction::Right:
-            return {coord.first, coord.second + 1};
-        case Direction::UpLeft:
-            return {coord.first - 1, coord.second - 1};
-        case Direction::UpRight:
-            return {coord.first - 1, coord.second + 1};
-        case Direction::DownLeft:
-            return {coord.first + 1, coord.second - 1};
-        case Direction::DownRight:
-            return {coord.first + 1, coord.second + 1};
-    }
-}
-
-int dfs(std::vector<std::vector<char>> &matrix, int i, int j, char last,
-        Direction direction)
-{
-    if (i < 0 || i >= matrix.size() || j < 0 || j >= matrix[0].size()) {
+    if (idx == 4) return true;
+    if (coords.first < 0 || coords.first >= matrix.size() ||
+        coords.second < 0 || coords.second >= matrix[0].size() ||
+        matrix[coords.first][coords.second] != XMAS[idx])
         return false;
-    }
-    int total = 0;
-    switch (last) {
-        case 'X': {
-            if (matrix[i][j] != 'M') return 0;
-            auto next_coord = get_next_coord({i, j}, direction);
-            return dfs(matrix, next_coord.first, next_coord.second, 'M',
-                       direction);
-        }
-        case 'M': {
-            if (matrix[i][j] != 'A') return false;
-            auto next_coord = get_next_coord({i, j}, direction);
-            return dfs(matrix, next_coord.first, next_coord.second, 'A',
-                       direction);
-        }
-        case 'A':
-            if (matrix[i][j] == 'S') {
-                std::cout << "Found XMAS. Direction: "
-                          << static_cast<int>(direction) << " end coord: " << i
-                          << ", " << j << '\n';
-            }
-            return matrix[i][j] == 'S';
-        default:
-            // Starting the search
-            total = dfs(matrix, i - 1, j, 'X', Direction::Up) +
-                    dfs(matrix, i + 1, j, 'X', Direction::Down) +
-                    dfs(matrix, i, j - 1, 'X', Direction::Left) +
-                    dfs(matrix, i, j + 1, 'X', Direction::Right) +
-                    dfs(matrix, i - 1, j - 1, 'X', Direction::UpLeft) +
-                    dfs(matrix, i - 1, j + 1, 'X', Direction::UpRight) +
-                    dfs(matrix, i + 1, j - 1, 'X', Direction::DownLeft) +
-                    dfs(matrix, i + 1, j + 1, 'X', Direction::DownRight);
-            break;
-    }
-    return total;
+    return func(matrix, update_lambda(coords.first, coords.second), idx + 1,
+                update_lambda);
 }
 
 int main()
@@ -104,7 +42,30 @@ int main()
     for (int i = 0; i < n_rows; ++i) {
         for (int j = 0; j < n_cols; ++j) {
             if (matrix[i][j] == 'X') {
-                count += dfs(matrix, i, j, '*', Direction::Up);
+                count += func(matrix, {i, j}, 0, [](int x, int y) {
+                    return std::pair<int, int>({x + 1, y});
+                });
+                count += func(matrix, {i, j}, 0, [](int x, int y) {
+                    return std::pair<int, int>({x - 1, y});
+                });
+                count += func(matrix, {i, j}, 0, [](int x, int y) {
+                    return std::pair<int, int>({x, y + 1});
+                });
+                count += func(matrix, {i, j}, 0, [](int x, int y) {
+                    return std::pair<int, int>({x, y - 1});
+                });
+                count += func(matrix, {i, j}, 0, [](int x, int y) {
+                    return std::pair<int, int>({x + 1, y + 1});
+                });
+                count += func(matrix, {i, j}, 0, [](int x, int y) {
+                    return std::pair<int, int>({x - 1, y + 1});
+                });
+                count += func(matrix, {i, j}, 0, [](int x, int y) {
+                    return std::pair<int, int>({x + 1, y - 1});
+                });
+                count += func(matrix, {i, j}, 0, [](int x, int y) {
+                    return std::pair<int, int>({x - 1, y - 1});
+                });
             }
         }
     }
